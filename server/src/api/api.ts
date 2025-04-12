6b74a79de3d997084d19c8f4c21037100ad9e48d
cairaaa <200700471+cairaaa@users.noreply.github.com>
2025-04-12 13:50:04 -0400

    added a function to get the status of a player

--- server/src/api/api.ts
+++ server/src/api/api.ts
@@ -24,6 +24,7 @@ async function getUUID(name: string): Promise<string> {
 }
 
 export async function getPlayer(name: string): Promise<object> {
+  // maybe change this later, to call on the uuid in the database?
   let uuid: string;
   try {
     uuid = await getUUID(name);
@@ -49,9 +50,28 @@ export async function getPlayer(name: string): Promise<object> {
   }
 }
 
-async function main() {
-  const player = await getPlayer("caiyy");
-  console.log(player);
+export async function getStatus(name: string): Promise<object> {
+  let uuid: string;
+  try {
+    uuid = await getUUID(name);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+  try {
+    const url = `https://api.hypixel.net/v2/status?key=${apiKey}&uuid=${uuid}`;
+    const response = await axios.get(url);
+    return response.data.session;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error.response?.status === 403) {
+        throw new Error("unable to retrieve player status, forbidden response");
+      } else if (error.response?.status === 404) {
+        throw new Error("unable to retrieve player status, invalid player");
+      } else if (error.response?.status === 429) {
+        throw new Error("unable to retrieve player status, too many requests");
+      }
+    }
+    throw new Error("unable to retrieve player status");
+  }
 }
-
-main();
