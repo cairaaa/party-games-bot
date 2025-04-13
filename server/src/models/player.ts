c4310beb23e806a16f1233914679269ff149883e
cairaaa <200700471+cairaaa@users.noreply.github.com>
2025-04-13 10:18:31 -0400

    changed code to use timestamps

--- server/src/models/player.ts
+++ server/src/models/player.ts
@@ -53,68 +53,68 @@ interface PlayerInterface extends Document {
       workshop: number;
     };
   };
-  firstLogin: number;
-  lastLogin: number;
-  created: Date;
-  updated: Date;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-const PlayerSchema: Schema = new Schema({
-  _id: { type: String, required: true },
-  username: { type: String, required: true },
-  stats: {
-    wins: { type: Number, required: true },
-    rounds: { type: Number, required: true },
-    stars: { type: Number, required: true },
-    pbs: {
-      anvilSpleefTime: { type: Number, required: true },
-      bombardmentTime: { type: Number, required: true },
-      chickenRingsTime: { type: Number, required: true },
-      jigsawRushTime: { type: Number, required: true },
-      jungleJumpTime: { type: Number, required: true },
-      labEscapeTime: { type: Number, required: true },
-      minecartRacingTime: { type: Number, required: true },
-      spiderMazeTime: { type: Number, required: true },
-      theFloorIsLavaTime: { type: Number, required: true },
-      animalSlaughterScore: { type: Number, required: true },
-      diveScore: { type: Number, required: true },
-      highGroundScore: { type: Number, required: true },
-      hoeHoeHoeScore: { type: Number, required: true },
-      lawnMoowerScore: { type: Number, required: true },
-      rpg16Score: { type: Number, required: true },
-    },
-    miniWins: {
-      animalSlaughter: { type: Number, required: true },
-      anvilSpleef: { type: Number, required: true },
-      avalanche: { type: Number, required: true },
-      bombardment: { type: Number, required: true },
-      cannonPainting: { type: Number, required: true },
-      chickenRings: { type: Number, required: true },
-      dive: { type: Number, required: true },
-      fireLeapers: { type: Number, required: true },
-      frozenFloor: { type: Number, required: true },
-      highGround: { type: Number, required: true },
-      hoeHoeHoe: { type: Number, required: true },
-      jigsawRush: { type: Number, required: true },
-      jungleJump: { type: Number, required: true },
-      labEscape: { type: Number, required: true },
-      lawnMoower: { type: Number, required: true },
-      minecartRacing: { type: Number, required: true },
-      pigFishing: { type: Number, required: true },
-      pigJousting: { type: Number, required: true },
-      rpg16: { type: Number, required: true },
-      shootingRange: { type: Number, required: true },
-      spiderMaze: { type: Number, required: true },
-      superSheep: { type: Number, required: true },
-      theFloorIsLava: { type: Number, required: true },
-      trampolinio: { type: Number, required: true },
-      volcano: { type: Number, required: true },
-      workshop: { type: Number, required: true },
+const PlayerSchema: Schema = new Schema(
+  {
+    _id: { type: String, required: true },
+    username: { type: String, required: true },
+    stats: {
+      wins: { type: Number, required: true },
+      rounds: { type: Number, required: true },
+      stars: { type: Number, required: true },
+      pbs: {
+        anvilSpleefTime: { type: Number, required: true },
+        bombardmentTime: { type: Number, required: true },
+        chickenRingsTime: { type: Number, required: true },
+        jigsawRushTime: { type: Number, required: true },
+        jungleJumpTime: { type: Number, required: true },
+        labEscapeTime: { type: Number, required: true },
+        minecartRacingTime: { type: Number, required: true },
+        spiderMazeTime: { type: Number, required: true },
+        theFloorIsLavaTime: { type: Number, required: true },
+        animalSlaughterScore: { type: Number, required: true },
+        diveScore: { type: Number, required: true },
+        highGroundScore: { type: Number, required: true },
+        hoeHoeHoeScore: { type: Number, required: true },
+        lawnMoowerScore: { type: Number, required: true },
+        rpg16Score: { type: Number, required: true },
+      },
+      miniWins: {
+        animalSlaughter: { type: Number, required: true },
+        anvilSpleef: { type: Number, required: true },
+        avalanche: { type: Number, required: true },
+        bombardment: { type: Number, required: true },
+        cannonPainting: { type: Number, required: true },
+        chickenRings: { type: Number, required: true },
+        dive: { type: Number, required: true },
+        fireLeapers: { type: Number, required: true },
+        frozenFloor: { type: Number, required: true },
+        highGround: { type: Number, required: true },
+        hoeHoeHoe: { type: Number, required: true },
+        jigsawRush: { type: Number, required: true },
+        jungleJump: { type: Number, required: true },
+        labEscape: { type: Number, required: true },
+        lawnMoower: { type: Number, required: true },
+        minecartRacing: { type: Number, required: true },
+        pigFishing: { type: Number, required: true },
+        pigJousting: { type: Number, required: true },
+        rpg16: { type: Number, required: true },
+        shootingRange: { type: Number, required: true },
+        spiderMaze: { type: Number, required: true },
+        superSheep: { type: Number, required: true },
+        theFloorIsLava: { type: Number, required: true },
+        trampolinio: { type: Number, required: true },
+        volcano: { type: Number, required: true },
+        workshop: { type: Number, required: true },
+      }
     }
   },
-  created: { type: Date, required: true },
-  updated: { type: Date, required: true }
-});
+  { timestamps: true }
+);
+
 
 const PlayerModel = model<PlayerInterface>("player", PlayerSchema);
 export default PlayerModel;
