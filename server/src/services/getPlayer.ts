e1392079f5426692a85b59b67f5e980fa25b82a4
cairaaa <200700471+cairaaa@users.noreply.github.com>
2025-04-14 18:05:49 -0400

    add function to get player stats from database

--- server/src/services/getPlayer.ts
+++ server/src/services/getPlayer.ts
@@ -1,9 +1,17 @@
-import { PlayerModel } from "../models/player";
+import { PlayerModel, PlayerInterface } from "../models/player";
 
 export async function getUUIDDatabase(name: string): Promise<string> {
   const player = await PlayerModel.findOne({ username: name });
   if (!player) {
-    throw new Error("player not found in database, can't get uuid");
+    throw new Error(`player ${name} not found in database, can't get uuid`);
   }
   return player._id;
+}
+
+export async function getPlayerDatabase(name: string): Promise<PlayerInterface> {
+  const player = await PlayerModel.findOne({ username: name });
+  if (!player) {
+    throw new Error(`player ${name} not found in database, can't get stats`);
+  }
+  return player;
 }
