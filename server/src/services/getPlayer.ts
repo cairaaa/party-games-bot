4f7d0b9ab872b5c1604705a61fffcbf8d54c9c5e
cairaaa <200700471+cairaaa@users.noreply.github.com>
2025-04-14 19:13:59 -0400

    add a function to see if a player was called recently

--- server/src/services/getPlayer.ts
+++ server/src/services/getPlayer.ts
@@ -15,3 +15,17 @@ export async function getPlayerDatabase(name: string): Promise<PlayerInterface> 
   }
   return player;
 }
+
+export async function getPlayerDatabaseRecent(name: string): Promise<PlayerInterface> {
+  const player = await PlayerModel.findOne({ username: name });
+  if (!player) {
+    throw new Error(`player ${name} not found in database, can't get stats`);
+  }
+  const now = new Date();
+  const updatedAt = player.updatedAt;
+  const time = now.getTime() - updatedAt!.getTime();
+  if (time > 30000) {
+    throw new Error("the data is not recent enough to return, please call on the hypixel api");
+  }
+  return player;
+}
