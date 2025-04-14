ea075e88403ede8d1557dae2b9215e30e8896820
cairaaa <200700471+cairaaa@users.noreply.github.com>
2025-04-14 16:44:17 -0400

    add PlayerType

--- server/src/models/player.ts
+++ server/src/models/player.ts
@@ -65,6 +65,69 @@ export interface PlayerInterface extends Document {
   updatedAt: Date;
 }
 
+export type PlayerType = {
+  _id: string;
+  username: string;
+  stats: {
+    wins: number;
+    rounds: number;
+    stars: number;
+    pbs: {
+      anvilSpleefTime: number;
+      bombardmentTime: number;
+      chickenRingsTime: number;
+      jigsawRushTime: number;
+      jungleJumpTime: number;
+      labEscapeTime: number;
+      minecartRacingTime: number;
+      spiderMazeTime: number;
+      theFloorIsLavaTime: number;
+      animalSlaughterScore: number;
+      diveScore: number;
+      highGroundScore: number;
+      hoeHoeHoeScore: number;
+      lawnMoowerScore: number;
+      rpg16Score: number;
+    };
+    miniWins: {
+      animalSlaughter: number;
+      anvilSpleef: number;
+      avalanche: number;
+      bombardment: number;
+      cannonPainting: number;
+      chickenRings: number;
+      dive: number;
+      fireLeapers: number;
+      frozenFloor: number;
+      highGround: number;
+      hoeHoeHoe: number;
+      jigsawRush: number;
+      jungleJump: number;
+      labEscape: number;
+      lawnMoower: number;
+      minecartRacing: number;
+      pigFishing: number;
+      pigJousting: number;
+      rpg16: number;
+      shootingRange: number;
+      spiderMaze: number;
+      superSheep: number;
+      theFloorIsLava: number;
+      trampolinio: number;
+      volcano: number;
+      workshop: number;
+    };
+    totals: {
+      animalSlaughterKills: number;
+      diveScore: number;
+      highGroundScore: number;
+      hoeHoeHoeScore: number;
+      lawnMoowerScore: number;
+      rpg16Kills: number;
+    };
+  };
+}
+
 const PlayerSchema: Schema = new Schema(
   {
     _id: { type: String, required: true },
