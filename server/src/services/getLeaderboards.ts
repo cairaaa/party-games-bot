485901ddce25acf711adb1330c55b1eea9babecd
cairaaa <200700471+cairaaa@users.noreply.github.com>
2025-04-14 22:56:41 -0400

    add function to get and sort the leaderboards

---
+++ server/src/services/getLeaderboards.ts
@@ -0,0 +1,27 @@
+import { LeaderboardModel, LeaderboardInterface } from "../models/leaderboard";
+import { Minigame, LbType } from "../types/types";
+
+export async function getLeaderboardDatabase(
+  minigame: Minigame, 
+  type: LbType
+  ): Promise<LeaderboardInterface> {
+  const leaderboard = await LeaderboardModel.findOne({ minigame, type });
+  if (!leaderboard) {
+    throw new Error(`couldnt find the ${type} ${minigame} leaderboard`);
+  }
+  return leaderboard;
+}
+
+export async function sortLeaderboard(
+  lb: LeaderboardInterface, 
+  order: "asc" | "desc" 
+  ): Promise<LeaderboardInterface> {
+  lb.players.sort((a, b) => {
+    if (order === "asc") {
+      return a.value - b.value;
+    } else {
+      return b.value - a.value;
+    }
+  });
+  return lb;
+}
