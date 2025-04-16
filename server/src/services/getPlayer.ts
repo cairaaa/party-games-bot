1b56d4991b9545383aaea37694a278f2edb4aa4d
cairaaa <200700471+cairaaa@users.noreply.github.com>
2025-04-16 19:41:28 -0400

    returns null instead of an error when a player is not found

--- server/src/services/getPlayer.ts
+++ server/src/services/getPlayer.ts
@@ -1,43 +1,43 @@
 import { PlayerModel, PlayerInterface } from "../models/player";
 
-export async function getUUIDDatabase(name: string): Promise<string> {
+export async function getUUIDDatabase(name: string): Promise<string | null> {
   const player = await PlayerModel.findOne(
     { username: name },
     null,
     { collation: { locale: "en", strength: 2 } }
   );
   if (!player) {
-    throw new Error(`player ${name} not found in database, can't get uuid`);
+    return null;
   }
   return player._id;
 }
 
-export async function getPlayerDatabase(name: string): Promise<PlayerInterface> {
+export async function getPlayerDatabase(name: string): Promise<PlayerInterface | null> {
   const player = await PlayerModel.findOne(
     { username: name },
     null,
     { collation: { locale: "en", strength: 2 } }
   );
   if (!player) {
-    throw new Error(`player ${name} not found in database, can't get stats`);
+    return null;
   }
   return player;
 }
 
-export async function getPlayerDatabaseRecent(name: string): Promise<PlayerInterface> {
+export async function getPlayerDatabaseRecent(name: string): Promise<PlayerInterface | null> {
   const player = await PlayerModel.findOne(
     { username: name },
     null,
     { collation: { locale: "en", strength: 2 } }
   );
   if (!player) {
-    throw new Error(`player ${name} not found in database, can't get stats`);
+    return null;
   }
   const now = new Date();
   const updatedAt = player.updatedAt;
   const time = now.getTime() - updatedAt!.getTime();
   if (time > 60000) {
-    throw new Error("the data is not recent enough to return, please call on the hypixel api");
+    return null;
   }
   return player;
 }
--- server/src/services/getStatus.ts
+++ server/src/services/getStatus.ts
@@ -1,13 +1,13 @@
 import { StatusInterface, StatusModel } from "../models/status";
 
-export async function getStatusDatabase(name: string): Promise<StatusInterface> {
+export async function getStatusDatabase(name: string): Promise<StatusInterface | null> {
   const player = await StatusModel.findOne(
-        { username: name },
-        null,
-        { collation: { locale: "en", strength: 2 } }
-      );
+    { username: name },
+    null,
+    { collation: { locale: "en", strength: 2 } }
+  );
   if (!player) {
-    throw new Error(`${name} was not found in the database, please call on hypixel api`);
+    return null;
   }
   return player;
 }
