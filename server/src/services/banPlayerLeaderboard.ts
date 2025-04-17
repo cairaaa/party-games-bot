410bb3a7b1489fb7d9abf146c34e5509697c1528
cairaaa <200700471+cairaaa@users.noreply.github.com>
2025-04-17 16:33:40 -0400

    make the functions not case sensitive

--- server/src/services/banPlayerLeaderboard.ts
+++ server/src/services/banPlayerLeaderboard.ts
@@ -5,7 +5,10 @@ export async function banLeaderboardsAll(name: string): Promise<void> {
   await LeaderboardModel.updateMany(
     { "players.username": name },
     { $set: { "players.$[player].banned": true } },
-    { arrayFilters: [{ "player.username": name }] }
+    { 
+      arrayFilters: [{ "player.username": name }],
+      collation: { locale: "en", strength: 2 }
+    }
   );
 }
 
@@ -16,25 +19,31 @@ export async function banLeaderboardOne(
   ): Promise<void> {
   await LeaderboardModel.updateOne(
     { minigame, type, "players.username": name },
-    { $set: { "players.$.banned": true } }
+    { $set: { "players.$.banned": true } },
+    { collation: { locale: "en", strength: 2 } }
   );
 }
 
 export async function unbanLeaderboardsAll(name: string): Promise<void> {
   await LeaderboardModel.updateMany(
     { "players.username": name },
-    { $unset: { "players.$.banned": "" } }
+    { $unset: { "players.$[player].banned": "" } },
+    { 
+      arrayFilters: [{ "player.username": name }],
+      collation: { locale: "en", strength: 2 }
+    }
   );
 }
 
-export async function unbanLeaderboardsOne(
+export async function unbanLeaderboardOne(
   name: string, 
   minigame: Minigame, 
   type: LbType
   ): Promise<void> {
   await LeaderboardModel.updateOne(
     { minigame, type, "players.username": name },
-    { $unset: { "players.$.banned": "" } }
+    { $unset: { "players.$.banned": "" } },
+    { collation: { locale: "en", strength: 2 } }
   );
 }
   
