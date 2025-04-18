6a57b2c0f08b6bc0a6ae40125fa9be5b47268f78
cairaaa <200700471+cairaaa@users.noreply.github.com>
2025-04-18 10:47:27 -0400

    add eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,21 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
+import stylisticTs from '@stylistic/eslint-plugin-ts'
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.ts"],
+    plugins: {
+      '@stylistic/ts': stylisticTs
+    },
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
+      "@stylistic/ts/comma-dangle": ["error", "never"],
       "semi": ["error", "always"],
       "quotes": ["error", "double"],
+      "no-empty": ["off"]
     }
   }
 );
