68bfd5faf566a96b0bf608ff15b52be39b879c30
cairaaa <200700471+cairaaa@users.noreply.github.com>
2025-04-19 10:32:09 -0400

    add cors and leaderboard router

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,20 @@
 import express, { Request, Response } from "express";
-import playerRouter from "./routes/playerRouter";
+import { playerRouter } from "./routes/playerRouter";
 import { connectToDatabase } from "./database/db";
+import cors from "cors";
+import { leaderboardRouter } from "./routes/leaderboardRouter";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use("/player", playerRouter);
+app.use(cors());
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("leafeon");
+});
+
+app.use("/players", playerRouter);
+app.use("/leaderboards", leaderboardRouter);
 
 export const connect = async (): Promise<void> => {
   try {
