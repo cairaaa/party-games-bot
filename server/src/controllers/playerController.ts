1ad99cd795ebacdb0f57a0e63d750647a1d12656
cairaaa <200700471+cairaaa@users.noreply.github.com>
2025-04-18 23:15:33 -0400

    change formatting, remove unneeded if block

--- server/src/controllers/playerController.ts
+++ server/src/controllers/playerController.ts
@@ -4,6 +4,7 @@ import { convertToPlayerModel, savePlayer } from "../services/savePlayer";
 import { getPlayerDatabaseRecent } from "../services/getPlayer";
 import { ApiResponse } from "../types";
 import { PlayerInterface } from "../models/player";
+import { saveLeaderboardAll } from "../services/saveLeaderboards";
 
 export const handleGetPlayer = async (
   req: Request, 
@@ -15,30 +16,29 @@ export const handleGetPlayer = async (
     res.status(200).json(checkDatabase);
     return;
   }
-  if (!checkDatabase.success) {
-    const apiResponse = await getPlayer(name);
-    if (apiResponse.success) {
-      res.status(200).json({ 
-        success: true,
-        data: convertToPlayerModel(apiResponse.data)
-      });
+  const apiResponse = await getPlayer(name);
+  if (apiResponse.success) {
+    const realData = convertToPlayerModel(apiResponse.data);
+    res.status(200).json({ 
+      success: true,
+      data: realData
+    });
+    await savePlayer(realData);
+    await saveLeaderboardAll(name);
+    return;
+  } else {
+    if (apiResponse.error.code === "FORBIDDEN_RESPONSE") {
+      res.status(403).json(apiResponse);
+      return;
+    } else if (apiResponse.error.code === "INVALID_PLAYER") {
+      res.status(404).json(apiResponse);
+      return;
+    } else if (apiResponse.error.code === "RATE_LIMIT_EXCEEDED") {
+      res.status(429).json(apiResponse);
       return;
     } else {
-      if (apiResponse.error.code === "FORBIDDEN_RESPONSE") {
-        res.status(403).json(apiResponse);
-        return;
-      } else if (apiResponse.error.code === "INVALID_PLAYER") {
-        res.status(404).json(apiResponse);
-        return;
-      } else if (apiResponse.error.code === "RATE_LIMIT_EXCEEDED") {
-        res.status(429).json(apiResponse);
-        return;
-      } else {
-        res.status(500).json(apiResponse);
-        return;
-      }
+      res.status(500).json(apiResponse);
+      return;
     }
   }
 };
-
-// add save player later i think
