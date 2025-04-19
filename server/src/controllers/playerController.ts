e4908a93ec45d3378765a1bd06ad43420cdd5ff6
cairaaa <200700471+cairaaa@users.noreply.github.com>
2025-04-19 17:34:36 -0400

    add try and catch block

--- server/src/controllers/playerController.ts
+++ server/src/controllers/playerController.ts
@@ -10,35 +10,45 @@ export const handleGetPlayer = async (
   req: Request, 
   res: Response<ApiResponse<PlayerInterface>>
 ): Promise<void> => {
-  const { name } = req.params;
-  const checkDatabase = await getPlayerDatabaseRecent(name);
-  if (checkDatabase.success) {
-    res.status(200).json(checkDatabase);
-    return;
-  }
-  const apiResponse = await getPlayer(name);
-  if (apiResponse.success) {
-    const realData = convertToPlayerModel(apiResponse.data);
-    res.status(200).json({ 
-      success: true,
-      data: realData
-    });
-    await savePlayer(realData);
-    await saveLeaderboardAll(name);
-    return;
-  } else {
-    if (apiResponse.error.code === "FORBIDDEN_RESPONSE") {
-      res.status(403).json(apiResponse);
-      return;
-    } else if (apiResponse.error.code === "INVALID_PLAYER") {
-      res.status(404).json(apiResponse);
-      return;
-    } else if (apiResponse.error.code === "RATE_LIMIT_EXCEEDED") {
-      res.status(429).json(apiResponse);
-      return;
-    } else {
-      res.status(500).json(apiResponse);
+  try {
+    const { name } = req.params;
+    const checkDatabase = await getPlayerDatabaseRecent(name);
+    if (checkDatabase.success) {
+      res.status(200).json(checkDatabase);
       return;
     }
+    const apiResponse = await getPlayer(name);
+    if (apiResponse.success) {
+      const realData = convertToPlayerModel(apiResponse.data);
+      res.status(200).json({ 
+        success: true,
+        data: realData
+      });
+      await savePlayer(realData);
+      await saveLeaderboardAll(name);
+      return;
+    } else {
+      if (apiResponse.error.code === "FORBIDDEN_RESPONSE") {
+        res.status(403).json(apiResponse);
+        return;
+      } else if (apiResponse.error.code === "INVALID_PLAYER") {
+        res.status(404).json(apiResponse);
+        return;
+      } else if (apiResponse.error.code === "RATE_LIMIT_EXCEEDED") {
+        res.status(429).json(apiResponse);
+        return;
+      } else {
+        res.status(500).json(apiResponse);
+        return;
+      }
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: {
+        message: "Unable to retrieve player stats",
+        code: "DATABASE_ERROR"
+      }
+    });
   }
 };
