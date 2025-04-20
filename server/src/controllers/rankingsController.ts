6084dd4907ec8bb4070723d5481b61d5e0946bad
cairaaa <200700471+cairaaa@users.noreply.github.com>
2025-04-20 12:21:29 -0400

    calls player data before giving rankings

--- server/src/controllers/rankingsController.ts
+++ server/src/controllers/rankingsController.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 import { ApiResponse } from "../types";
 import { getRankings, getRealRankings, RankingInterface } from "../services/getRankings";
+import { getPlayer } from "../api";
+import { convertToPlayerModel, savePlayer } from "../services/savePlayer";
+import { saveLeaderboardAll } from "../services/saveLeaderboards";
+import { getPlayerDatabase, getPlayerDatabaseRecent } from "../services/getPlayer";
 
 const handleGetRankings = async (
   req: Request, 
@@ -10,19 +14,47 @@ const handleGetRankings = async (
   try {
     const { name } = req.params;
     let rankings: ApiResponse<RankingInterface>;
-    if (useFilter) {
-      rankings = await getRealRankings(name);
-    } else {
-      rankings = await getRankings(name);
-    }
-    if (rankings.success) {
+    const checkDatabase = await getPlayerDatabaseRecent(name);
+    if (checkDatabase.success) {
+      if (useFilter) {
+        rankings = await getRealRankings(name);
+      } else {
+        rankings = await getRankings(name);
+      }
       res.status(200).json(rankings);
       return;
-    } else {
-      if (rankings.error.code === "INVALID_PLAYER") {
-        res.status(404).json(rankings);
-        return;
+    }
+    const playerData = await getPlayer(name);
+    if (playerData.success) {
+      const savePlayerData = convertToPlayerModel(playerData.data);
+      const databaseData = await getPlayerDatabase(name);
+      if (databaseData.success) {
+        const existingPbs = databaseData.data.stats.pbs;
+        if (JSON.stringify(existingPbs) !== JSON.stringify(savePlayerData.stats.pbs)) {
+          await savePlayer(savePlayerData);
+          await saveLeaderboardAll(name);
+          if (useFilter) {
+            rankings = await getRealRankings(name);
+          } else {
+            rankings = await getRankings(name);
+          }
+          res.status(200).json(rankings);
+          return;
+        }
       }
+      if (useFilter) {
+        rankings = await getRealRankings(name);
+      } else {
+        rankings = await getRankings(name);
+      }
+      res.status(200).json(rankings);
+      await savePlayer(savePlayerData);
+      await saveLeaderboardAll(name);
+      return;
+    }
+    if (playerData.error.code === "INVALID_PLAYER") {
+      res.status(404).json(playerData);
+      return;
     }
     res.status(500).json({
       success: false,
