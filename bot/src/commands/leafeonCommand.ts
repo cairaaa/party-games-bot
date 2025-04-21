2ec8fa786707b9a0ebfeee1b248f4bafeb7c081f
cairaaa <200700471+cairaaa@users.noreply.github.com>
2025-04-21 11:57:11 -0400

    add a test command

---
+++ bot/src/commands/leafeonCommand.ts
@@ -0,0 +1,11 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { Command } from "../types/Command";
+
+export const leafeonCommand: Command = {
+  data: new SlashCommandBuilder()
+   .setName("leafeon")
+   .setDescription("replies with leafeon, a test command"),
+  async execute(interaction: ChatInputCommandInteraction) {
+    interaction.reply("leafeon");
+  }
+};
