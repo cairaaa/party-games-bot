ed25ba0656e2947e9f5e9510ccc4b56b2b5cb3ed
cairaaa <200700471+cairaaa@users.noreply.github.com>
2025-04-24 22:37:45 -0400

    fix increationCreate, add a client

--- bot/src/events/interactionCreate.ts
+++ bot/src/events/interactionCreate.ts
@@ -3,19 +3,24 @@ import { Client, Events, Interaction } from "discord.js";
 export const interactionCreateEvent = {
   name: Events.InteractionCreate,
   once: false,
-  async execute(interaction: Interaction, client: Client) {
+  async execute(interaction: Interaction) {
 		if (!interaction.isChatInputCommand()) {
+      return;
+    }
+    const client = interaction.client; 
+    if (!client.commands) {
+      console.log("there are no commands initialized");
       return;
     }
     const command = client.commands.get(interaction.commandName);
     if (!command) {
-      console.error(`${interaction.commandName} does not exist`);
+      console.log(`${interaction.commandName} does not exist`);
       return;
     }
     try {
       await command.execute(interaction);
     } catch (error) {
-      console.error(error);
+      console.log(error);
       // if (interaction.replied || interaction.deferred) {
       //   await interaction.followUp({ 
       //     content: "there was an error while executing this command", 
