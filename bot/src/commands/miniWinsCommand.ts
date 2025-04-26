725d0cd8fcb61877202bcf8f9fed6235f671b7dc
cairaaa <200700471+cairaaa@users.noreply.github.com>
2025-04-26 18:35:25 -0400

    add command to get minigame wins

---
+++ bot/src/commands/miniWinsCommand.ts
@@ -0,0 +1,29 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder, AttachmentBuilder } from "discord.js";
+import { Command } from "../types/Command";
+import { createMiniWinsCanvas } from "../canvas/miniWinsCanvas";
+
+export const miniWinsCanvas: Command = {
+  data: new SlashCommandBuilder()
+    .setName("mini")
+    .setDescription("get minigame wins")
+    .addStringOption(option =>
+      option.setName("player")
+      .setDescription("enter a player")
+      .setRequired(true)
+    ),
+  async execute(interaction: ChatInputCommandInteraction) {
+    try {
+      const name = interaction.options.getString("player", true);
+      const imageBuffer = await createMiniWinsCanvas(name);
+      if (typeof imageBuffer === "string") {
+        await interaction.reply(imageBuffer);
+        return;
+      }
+      const attachment = new AttachmentBuilder(imageBuffer, { name: `${name}-mini.jpg` });
+      await interaction.reply({ files: [attachment] });
+    } catch (error) {
+      console.log(error);
+      await interaction.reply("There was an error while getting the player's minigame wins");
+    }
+  }
+};
