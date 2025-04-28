3760da4009b82272826d0bc8bc2d7c855f7ee8f9
cairaaa <200700471+cairaaa@users.noreply.github.com>
2025-04-27 22:10:03 -0400

    add autocomplete interaction handling

--- bot/src/events/interactionCreate.ts
+++ bot/src/events/interactionCreate.ts
@@ -1,37 +1,41 @@
-import { Client, Events, Interaction } from "discord.js";
+import { Events, Interaction } from "discord.js";
 
 export const interactionCreateEvent = {
   name: Events.InteractionCreate,
   once: false,
   async execute(interaction: Interaction): Promise<void> {
-		if (!interaction.isChatInputCommand()) {
-      return;
-    }
-    const client = interaction.client; 
+    const client = interaction.client;
     if (!client.commands) {
-      console.log("there are no commands initialized");
+      console.log("There are no commands initialized");
       return;
     }
-    const command = client.commands.get(interaction.commandName);
-    if (!command) {
-      console.log(`${interaction.commandName} does not exist`);
+    if (interaction.isAutocomplete()) {
+      const command = client.commands.get(interaction.commandName);
+      if (!command) {
+        console.log(`${interaction.commandName} does not exist`);
+        return;
+      }
+      try {
+        if (command.autocomplete) {
+          await command.autocomplete(interaction);
+        }
+      } catch (error) {
+        console.log("Error in autocomplete:", error);
+      }
       return;
     }
-    try {
-      await command.execute(interaction);
-    } catch (error) {
-      console.log(error);
-      // if (interaction.replied || interaction.deferred) {
-      //   await interaction.followUp({ 
-      //     content: "there was an error while executing this command", 
-      //     flags: MessageFlags.Ephemeral 
-      //   });
-      // } else {
-      //   await interaction.reply({ 
-      //     content: "there was an error while executing this command", 
-      //     flags: MessageFlags.Ephemeral 
-      //   });
-      // }
+    if (interaction.isChatInputCommand()) {
+      const command = client.commands.get(interaction.commandName);
+      if (!command) {
+        console.log(`${interaction.commandName} does not exist`);
+        return;
+      }
+      try {
+        await command.execute(interaction);
+      } catch (error) {
+        console.log("Error executing command:", error);
+      }
+      return;
     }
   }
 };
