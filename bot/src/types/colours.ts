f84824894871bc73415f69b798ccc1779b6763a5
cairaaa <200700471+cairaaa@users.noreply.github.com>
2025-04-29 23:19:49 -0400

    add more colours, specifically for lb

--- bot/src/types/colours.ts
+++ bot/src/types/colours.ts
@@ -1,5 +1,8 @@
 export enum colour {
   white = "#ffffff",
   green = "#8ee3b8",
-  lightGreen = "#c9f5de"
+  lightGreen = "#c9f5de",
+  gold = "#ffbf00",
+  silver = "#d2d3d4",
+  bronze = "#dea474"
 }
