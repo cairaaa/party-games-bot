11515887f781204c961513d69a6413bd99ce9e4e
cairaaa <200700471+cairaaa@users.noreply.github.com>
2025-04-30 23:13:42 -0400

    add command for total scores

---
+++ bot/src/commands/totalsCommand.ts
@@ -0,0 +1,31 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder, AttachmentBuilder } from "discord.js";
+import { Command } from "../types/Command";
+import { createTotalsCanvas } from "../canvas/totalsCanvas";
+
+export const totalsCommand: Command = {
+  data: new SlashCommandBuilder()
+    .setName("totals")
+    .setDescription("get total scores")
+    .addStringOption(option =>
+      option.setName("player")
+      .setDescription("enter a player")
+      .setRequired(true)
+    ),
+  async execute(interaction: ChatInputCommandInteraction) {
+    try {
+      console.time("hi");
+      const name = interaction.options.getString("player", true);
+      const imageBuffer = await createTotalsCanvas(name);
+      if (typeof imageBuffer === "string") {
+        await interaction.reply(imageBuffer);
+        return;
+      }
+      const attachment = new AttachmentBuilder(imageBuffer, { name: `${name}-totals.jpg` });
+      await interaction.reply({ files: [attachment] });
+      console.timeEnd("hi");
+    } catch (error) {
+      console.log(error);
+      await interaction.reply("There was an error while getting the player's total scores");
+    }
+  }
+};
