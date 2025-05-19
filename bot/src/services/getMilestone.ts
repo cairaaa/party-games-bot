17a104df3da8c8baa7c250524998bf3eacb9ebe7
cairaaa <200700471+cairaaa@users.noreply.github.com>
2025-05-19 14:05:13 -0400

    change so you can input your own interval

--- bot/src/services/getMilestone.ts
+++ bot/src/services/getMilestone.ts
@@ -2,11 +2,21 @@ import { callPlayer } from "./callServer";
 
 export async function getMilestone(name: string): Promise<string> {
   const response = await callPlayer(name);
+  const [_player, winsInterval] = name.split(" ");
+  let realWinsInterval: number;
+  if (!winsInterval) {
+    realWinsInterval = 500;
+  } else {
+    realWinsInterval = Number(winsInterval);
+  }
+  if (isNaN(realWinsInterval) || (name.split(" ").length - 1) > 2) {
+    return "Please provide a valid input (1 username with a number seperated by a space)";
+  }
   if (!response.success) {
     return response.error.message;
   }
   const p = response.data;
   const wins = p.stats.wins;
-  const winsNeeded = Math.ceil(wins / 500) * 500;
+  const winsNeeded = Math.ceil(wins / realWinsInterval) * realWinsInterval;
   return `${p.username} needs **${winsNeeded - wins}** more wins for the next milestone at **${winsNeeded}** wins`;
 }
